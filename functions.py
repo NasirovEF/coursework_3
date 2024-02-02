f7fed43b870cf8da4e4092a3d14517b62c60fad3
NasirovEF <i@ef-nasirov.ru>
2024-02-03 01:37:36 +0300

    add new function, but i don't know will i used it or not

--- functions.py
+++ functions.py
@@ -5,11 +5,22 @@ def read_file(operations):
     """Читает Json файл и возвращает список"""
 
     with open(operations, encoding="utf8") as file:
-        list_operation = json.load(file)
+        list_operations = json.load(file)
 
-    return list_operation
+    return list_operations
+
+
+def list_oper(list_operations):
+
+    list_1 = []
+    for operation in list_operations:
+        list_1.append(operation)
+
+    print(list_1.sort(key=operation))
+
+
+
+
 
 operations = 'operations.json'
-list_operation = read_file(operations)
-print(list_operation)
-
+list_oper(read_file(operations))
