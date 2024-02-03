b720e26c79411a61828149b118398756491f02c1
NasirovEF <i@ef-nasirov.ru>
2024-02-03 18:31:15 +0300

    add new finction (transfer_amount) and str in Classes.py

--- Classes.py
+++ Classes.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 class Operation:
 
-    def __init__(self, id_op, date, state, op_am, dscr, fr, to):
+    def __init__(self, id_op, state, date, op_am, dscr, fr, to):
         self.id = id_op
         self.date = date
         self.state = state
@@ -28,11 +28,18 @@ class Operation:
 
         return f'{the_right_date.day}.{the_right_date.month}.{the_right_date.year}'
 
+    def transfer_amount(self):
+        """Возвращает сумму перевода с указанием валюты"""
+
+        return f"{self.op_am["amount"]} {self.op_am["currency"]["name"]}"
+
     def hide_inform(self, deposit):
         """Маскирует номер карты и номер счета"""
 
         if "Счет" in deposit:
             return f'Счет **{deposit[-4:]}'
+        elif len(deposit) == 0:
+            return ""
         else:
             share = deposit.split()
             name_card = []
@@ -44,3 +51,11 @@ class Operation:
                     name_card.append(i)
             return f'{" ".join(name_card)} {card_numb[0][-16:-12]} {card_numb[0][-16:-14]}** **** {card_numb[0][-4:]}'
 
+    def __str__(self):
+        return f"{self.right_date()} {self.dscr}\n{self.hide_inform(self.fr)} -> {self.hide_inform(self.to)}\n{self.transfer_amount()}"
+
+
+first = Operation(547682597, "EXECUTED", "2018-12-29T21:45:18.495053", {"amount": "66263.93", "currency": {"name": "руб.", "code": "RUB"}}, "Перевод организации", "Счет 77977573135347241529", "Счет 33062909508148771891")
+
+
+print(first)
