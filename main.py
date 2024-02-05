8c0ca389d6b28d4c5344c597fa68d6dbea019e8d
NasirovEF <i@ef-nasirov.ru>
2024-02-05 20:20:46 +0300

    add main

---
+++ main.py
@@ -0,0 +1,15 @@
+from functions import *
+from Classes import Operation
+
+operations = "operations.json"
+
+def main():
+    """Функция выводит 5 последних успешно выполненных
+    операций отсортированных по дате"""
+
+    list_operations = sorted_list(list_time_state(read_file(operations)))
+    for operation in list_operations:
+        massage = Operation(operation)
+        print(massage)
+
+main()
